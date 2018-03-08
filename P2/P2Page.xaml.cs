9f7b474db757cf3bd2c4e94d5c331d459d247ee0
bastianhoj <bastianhoejbjerre@gmail.com>
2018-03-08 13:46:00 +0100

    First commit

---
+++ P2/P2Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace P2
+{
+    public partial class P2Page : TabbedPage
+    {
+        public P2Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
